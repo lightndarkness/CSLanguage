22657c7cb0c49c475dfe51766feb9b7a6953b4e9
Casole Giovanni <casolegiovanni@live.it>
2016-04-02 09:58:55 +0200

    C#

---
+++ MicrosoftCSstepByStep/Chapter 1/TestHello - Complete/TestHello/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    class Program
+    {
+        enum Error { DEFAULT, STRINGA, ZERO };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("        Option Chooser        \n\n");
+
+            char userAnswer;
+            
+            do
+            {
+                int max = 0;
+                do
+                {
+                    Error error = Error.DEFAULT;
+                    
+                    Console.WriteLine("Quante opzioni hai?");
+                    try
+                    {
+                        max = Convert.ToInt32(Console.ReadLine());
+                        if (max == 0)
+                        {
+                            error = Error.ZERO;
+                        }
+                    }
+                    catch (FormatException fe)
+                    {
+                        Console.WriteLine(fe.Message);
+                        error = Error.STRINGA;
+                    }
+                    switch (error)
+                    {
+                        case Error.STRINGA:
+                            Console.WriteLine("Devi inserire un numero!");
+                            break;
+                        case Error.ZERO:
+                            Console.WriteLine("Non è possibile avere 0 opzioni.");
+                            break;
+                        case Error.DEFAULT:
+                        default:
+                            break;
+                    }
+                } while (max == 0);
+
+                Console.WriteLine("Hai " + max + " opzioni.");
+                Random random = new Random();
+
+                int opzione = random.Next(1, max + 1);
+
+                Console.WriteLine("L'opzione scelta è la " + opzione + ".");
+                Console.WriteLine("Vuoi generare un'altra opzione? (y/n)");
+                userAnswer = Convert.ToChar(Console.ReadLine());
+            } while (userAnswer == 'y' || userAnswer == 'Y');
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int iMax;
+    int iMin;
+    int iGuess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // print("up arrow key was pressed");
+            iMin = iGuess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // print("down arrow key pressed");
+            iMax = iGuess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!");
+            
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        iMax = 1000;
+        iMin = 1;
+        iGuess = 500;
+
+        print("========================");
+        print("Welcome to Number Wizard");
+        print("Pick a number in your head, but don't tell me");
+
+        print("The highest number you can pick is " + iMax);
+        print("The lowest number you can pick is " + iMin);
+
+        print("Is the number higher or lower than " + iGuess + "?");
+        print("Press UpArrow if higher, DownArrow if lower, Enter if equal");
+        iMax++;
+    }
+
+    void NextGuess()
+    {
+        iGuess = (iMax + iMin) / 2;
+        print("Higher or lower than " + iGuess);
+        print("Press UpArrow if higher, DownArrow if lower, Enter if equal");
+    }
+}
